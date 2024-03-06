62e839974174121d1f51be13476570179f013966
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2024-03-06 16:18:00 -0600

    Add Application Project

--- src/CleanArchitecture/CleanArchitecture.Application/obj/Debug/net8.0/CleanArchitecture.Application.AssemblyInfo.cs
+++ src/CleanArchitecture/CleanArchitecture.Application/obj/Debug/net8.0/CleanArchitecture.Application.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchitecture.Domain")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchitecture.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c0c2551e792bce3a0c1921931fa7e20f82eb03")]
-[assembly: System.Reflection.AssemblyProductAttribute("CleanArchitecture.Domain")]
-[assembly: System.Reflection.AssemblyTitleAttribute("CleanArchitecture.Domain")]
+[assembly: System.Reflection.AssemblyProductAttribute("CleanArchitecture.Application")]
+[assembly: System.Reflection.AssemblyTitleAttribute("CleanArchitecture.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
