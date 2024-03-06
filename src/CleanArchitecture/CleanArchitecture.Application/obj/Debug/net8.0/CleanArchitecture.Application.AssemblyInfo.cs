ea12bbde7a249f4de3a17f6b5555a6f734fb27c4
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2024-03-06 16:39:52 -0600

    Configurando MediatR y creando abstraciones

--- src/CleanArchitecture/CleanArchitecture.Application/obj/Debug/net8.0/CleanArchitecture.Application.AssemblyInfo.cs
+++ src/CleanArchitecture/CleanArchitecture.Application/obj/Debug/net8.0/CleanArchitecture.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchitecture.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c0c2551e792bce3a0c1921931fa7e20f82eb03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62e839974174121d1f51be13476570179f013966")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchitecture.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchitecture.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
