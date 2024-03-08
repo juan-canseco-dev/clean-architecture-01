b6c29aa80e34be33592f75faa611bcb06273330d
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2024-03-07 18:28:07 -0600

    Agregar Vehiculos en Application

---
+++ src/CleanArchitecture/CleanArchitecture.Application/Alquileres/GetAlquiler/GetAlquilerQuery.cs
@@ -0,0 +1,7 @@
+using CleanArchitecture.Application.Abstractions.Messaging;
+
+namespace CleanArchitecture.Application.Alquileres.GetAlquiler;
+
+public sealed record GetAlquilerQuery(
+    Guid AlquilerId
+) : IQuery<AlquilerResponse>;
--- src/CleanArchitecture/CleanArchitecture.Application/obj/Debug/net8.0/CleanArchitecture.Application.AssemblyInfo.cs
+++ src/CleanArchitecture/CleanArchitecture.Application/obj/Debug/net8.0/CleanArchitecture.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchitecture.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea12bbde7a249f4de3a17f6b5555a6f734fb27c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+577fc28d24f0ee88aaf0a6a8c2f7ed0ad6a4a4c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchitecture.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchitecture.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
